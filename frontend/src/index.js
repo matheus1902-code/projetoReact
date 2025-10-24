3de53f38e6be7b66aeb754761c1aabb400d76bad
matheus1902-code <matheusvm2019@gmail.com>
2025-10-24 12:17:51 -0300

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import app from './main/App'
+import registerServiceWorker from './registerServiceWorker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+registerServiceWorker();
---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import 'boorstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min';
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from '../auth/AuthContent';
+import PrivateRoute from '../auth/PrivateRoute';
+
+//Componentes principais
+import Logo from '../components/template/Logo';
+import Nav from '../components/template/Nav';
+
+// Páginas
+import Login from '../components/user/LoginForm';
+import Register from '../components/user/Register';
+
+import UserCrud from '../components/user/UserCrud';
+import Home from '../components/user/Home';
+import Footer from '../components/template/Footer';
+
+function App(){
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="app"> {/* layout principal com grid e sem fundo branco */}
+        {/*  Exibe a estrutura fixa apenas se o usuário estiver logado*/}
+          <PrivateRoute>
+            <Logo/ >
+            <Nav/ >
+          </PrivateRoute>   
+          <main className="app-content">
+            <Routes>
+              {/* Rotas públicas */}
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/register" element={<Register/>}/>
+
+              {/* Rotas privadas  */}
+              <Route 
+                path="/"
+
+                element={
+                  <PrivateRoute>
+                    <Home />
+                  </PrivateRoute>
+                }
+              />
+
+              <Route
+                
+              />
+
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  )
+}
